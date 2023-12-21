bcdeb478759547e7f9384a9f3025d0872d26eafb
emma-25 <mombellitic@gmail.com>
2023-12-21 17:36:04 -0300

    first commit

---
+++ apps/agencia/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import ListView
+from .models import Auto
+
+class AutoListView(ListView):
+    model = Auto
+    template_name = 'agencia/auto/lista_autos.html'
+    context_object_name = 'autos'
+
+
+
+
+
+
